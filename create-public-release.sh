0b1daa59272a3030b6b8597f63cc0f81126e7b4e
Simon Levesque <simon@foilen.com>
2021-02-07 18:48:41 -0500

    Switch to Maven Central

--- create-public-release.sh
+++ create-public-release.sh
@@ -7,26 +7,35 @@ cd $RUN_PATH
 
 # Check params
 if [ $# -ne 1 ]
-	then
-		echo Usage: $0 version;
-    echo E.g: $0 0.1.0
-		echo Version is MAJOR.MINOR.BUGFIX
-		echo Latest version:
-		git describe --abbrev=0
-		exit 1;
+    then
+        echo Usage: $0 version;
+        echo E.g: $0 0.1.0
+        echo Version is MAJOR.MINOR.BUGFIX
+        echo Latest version:
+        git describe --abbrev=0
+        exit 1;
 fi
 
+# Set environment
+export LANG="C.UTF-8"
 export VERSION=$1
 
 ./step-update-copyrights.sh
 ./step-clean-compile.sh
 ./step-create-docker-image.sh
 ./step-upload-docker-image.sh
-./step-upload-bintray.sh
+./step-upload-ossrh.sh
 ./step-git-tag.sh
 
 echo ----[ Operation completed successfully ]----
 
 echo
+echo To deploy the new release:
+echo '- go on https://oss.sonatype.org/#stagingRepositories'
+echo '- close it (wait for it to be closed)'
+echo '- release it'
+echo 
+echo Then, can see published items on 
+echo https://repo1.maven.org/maven2/com/foilen/database-tools/
 echo You can see published items on https://hub.docker.com/r/foilen/database-tools/tags/
 echo You can send the tag: git push --tags

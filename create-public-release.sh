7dcffb5022bd4d7fde6446915c94958b6c7a8707
Simon Levesque <simon@foilen.com>
2026-09-03 13:28:02 -0400

    Upgrade to Java 21 with Maven Central fixes and latest libraries

--- create-public-release.sh
+++ create-public-release.sh
@@ -22,19 +22,16 @@ export VERSION=$1
 
 ./step-clean-compile.sh
 ./step-create-docker-image.sh
-./step-upload-ossrh.sh
+./step-upload-maven-central.sh
 ./step-upload-docker-image.sh
 ./step-git-tag.sh
 
 echo ----[ Operation completed successfully ]----
 
 echo
-echo To deploy the new release:
-echo '- go on https://oss.sonatype.org/#stagingRepositories'
-echo '- close it (wait for it to be closed)'
-echo '- release it'
-echo 
-echo Then, can see published items on 
+echo You can see the publishing progress on https://central.sonatype.com/publishing/deployments
+echo
+echo Then, can see published items on
 echo https://repo1.maven.org/maven2/com/foilen/database-tools/
 echo You can see published items on https://hub.docker.com/r/foilen/database-tools/tags/
 echo You can send the tag: git push --tags
--- src/test/java/com/foilen/databasetools/connection/JdbcUriConfigConnectionTest.java
+++ src/test/java/com/foilen/databasetools/connection/JdbcUriConfigConnectionTest.java
@@ -1,31 +1,31 @@
 package com.foilen.databasetools.connection;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class JdbcUriConfigConnectionTest {
 
     @Test
     public void testToString_EndsWithPassword() {
-        Assert.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?password=*****&user=root]",
+        Assertions.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?password=*****&user=root]",
                 new JdbcUriConfigConnection().setJdbcUri("jdbc:mariadb://localhost:3306/DB?user=root&password=myPassword").toString());
     }
 
     @Test
     public void testToString_WithoutPassword() {
-        Assert.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?user=root]",
+        Assertions.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?user=root]",
                 new JdbcUriConfigConnection().setJdbcUri("jdbc:mariadb://localhost:3306/DB?user=root").toString());
     }
 
     @Test
     public void testToString_WithPassword() {
-        Assert.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?connectTimeout=10000&password=*****&user=root]",
+        Assertions.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mariadb://localhost:3306/DB?connectTimeout=10000&password=*****&user=root]",
                 new JdbcUriConfigConnection().setJdbcUri("jdbc:mariadb://localhost:3306/DB?user=root&password=myPassword&connectTimeout=10000").toString());
     }
 
     @Test
     public void testToString_WithPasswordInUserinfo() {
-        Assert.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mongodb://root:*****@localhost/admin]",
+        Assertions.assertEquals("JdbcUriConfigConnection [jdbcUri=jdbc:mongodb://root:*****@localhost/admin]",
                 new JdbcUriConfigConnection().setJdbcUri("jdbc:mongodb://root:ABC@localhost/admin").toString());
     }
 
